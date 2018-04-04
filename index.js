05613b9442e7c1cb04a812e05a924b4aff3bad16
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-04 15:08:54 -0700

    Basic BIP44 wallet.

--- index.js
+++ index.js
@@ -7,8 +7,66 @@ let BITBOX = new BITBOXCli({
   password: ''
 });
 
-BITBOX.Control.getInfo()
-.then((result) => {
-  console.log(result);
-}, (err) => { console.log(err);
-});
+let langs = [
+  'english',
+  'chinese_simplified',
+  'chinese_traditional',
+  'korean',
+  'japanese',
+  'french',
+  'italian',
+  'spanish'
+]
+
+let lang = langs[Math.floor(Math.random()*langs.length)];
+
+// create 256 bit BIP39 mnemonic
+let mnemonic = BITBOX.Mnemonic.generateMnemonic(256, BITBOX.Mnemonic.mnemonicWordLists()[lang])
+console.log("BIP44 $BCH Wallet");
+console.log(`256 bit ${lang} BIP39 Mnemonic: `, mnemonic);
+
+// mnemonic to BIP32 root seed encoded as hex
+let rootSeedHex = BITBOX.Mnemonic.mnemonicToSeedHex(mnemonic)
+
+// root seed to BIP32 master HD Node
+let masterHDNode = BITBOX.HDNode.fromSeedHex(rootSeedHex)
+
+// derive BIP 44 external receive address
+let childNode = masterHDNode.derivePath("m/44'/145'/0'/0/0")
+console.log(`BIP44 Account: "m/44'/145'/0'"`);
+for(let i = 0; i < 10; i++) {
+  let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
+  console.log(`m/44'/145'/0'/0/${i}: ${BITBOX.HDNode.toCashAddress(childNode)}`);
+}
+
+
+// instance of transaction builder
+let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
+
+// keypair of BIP44 receive address
+let keyPair = childNode.keyPair;
+
+// txid of utxo
+let txid = '5699610b1db28d77b1021ed457d5d9010900923143757bc8698083fa796b3307';
+
+// add input txid, vin 1 and keypair
+transactionBuilder.addInput(txid, 1, keyPair);
+
+// calculate fee @ 1 sat/B
+let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
+
+// subtract fee from original amount
+let originalAmount = 3678031;
+
+let sendAmount = originalAmount - byteCount;
+
+// add receive address and send amount
+transactionBuilder.addOutput('qpq57nsrhje3725fzxfjdqzdngep3cfk2sfmy8yexj', sendAmount);
+
+// sign tx
+transactionBuilder.sign(0, originalAmount);
+
+// build it and raw hex
+let tx = transactionBuilder.build();
+let hex = tx.toHex();
+console.log(`Transaction raw hex: ${hex}`);
