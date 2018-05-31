1470b4a9c357bdd0ba5139e8f4332904d042ce3b
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-31 08:25:49 -0700

    Version and show txid.

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
-let BITBOX = new BITBOXCli({
-  protocol: 'http',
-  host: '127.0.0.1',
-  port: 8332,
-  username: '',
-  password: '',
-  corsproxy: 'remote'
-});
+let BITBOX = new BITBOXCli();
 
 let langs = [
   'english',
@@ -92,7 +85,7 @@ BITBOX.Address.utxo(cashAddress).then((result) => {
 
   // sendRawTransaction to running BCH node
   BITBOX.RawTransactions.sendRawTransaction(hex).then((result) => {
-    console.log(result);
+    console.log(`Transaction ID: ${result}`);
   }, (err) => {
     console.log(err);
   });
